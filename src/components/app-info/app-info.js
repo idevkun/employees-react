94db495943cd14559af08685bb5c66cf2688668d
Aybek Amanbaev <idevkun@gmail.com>
2022-04-06 22:40:57 +0500

    added form

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,7 +1,7 @@
 import './app-info.css';
 const AppInfo = () => {
     return (
-        <div className='app-info'>
+        <div className='app-info mt-3'>
             <h1>Учет сотрудников в компании N</h1>
             <h2>Общее число сотрудников</h2>
             <h2>Премию у нас получат</h2>
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployessList from '../employees-list/employess-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 function App() {
     return (
         <div className="app">
@@ -13,6 +14,7 @@ function App() {
                 <AppFilter/>
             </div>
                 <EmployessList/>
+                <EmployeesAddForm/>
         </div>
     )
 }
